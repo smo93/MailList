84bcbbef2d089d4179c8c6df58b0a5850a38fe46
smo93 <georgimpeev@gmail.com>
2014-03-18 21:56:08 +0200

    main func added

--- mail.py
+++ mail.py
@@ -37,3 +37,41 @@ def search_email(lists, email):
             result.append('[{0}] - {1}'.format(key, lists[key].get_name()))
     return '\n'.join(result)
 
+def parse_command(command):
+    return tuple(command.split(' '))
+
+def is_command(command_tuple, command_string):
+    return command_tuple[0] == command_string
+
+def main():
+    lists = {}
+
+    print(create_menu())
+
+    while True:
+        command_tuple = parse_command(input('Enter command>'))
+
+        if is_command(command_tuple, 'help'):
+            print(create_help())
+        elif is_command(command_tuple, 'show_lists'):
+            print(show_lists(lists))
+        elif is_command(command_tuple, 'show_list'):
+            print(show_list(lists, int(command_tuple[1])))
+        elif is_command(command_tuple, 'add'):
+            name = input('name>')
+            email = input('email>')
+            add_new_user(lists, int(command_tuple[1]), name, email)
+        elif is_command(command_tuple, 'create'):
+            create_list(lists, command_tuple[1])
+        elif is_command(command_tuple, 'search_email'):
+            print(search_email(lists, command_tuple[1]))
+        elif is_command(command_tuple, 'merge_lists'):
+            list1, list2, new_list = command_tuple[1].split(' ')
+            pass
+        elif is_command(command_tuple, 'export'):
+            pass
+        elif is_command(command_tuple, 'exit'):
+            break
+        else:
+            print('Unknown command!')
+
