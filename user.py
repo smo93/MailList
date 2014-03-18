fe4797e9ddb9e1d910efec29b1ac31de76b47c7d
plammfish <plampetrova83@gmail.com>
2014-03-18 19:46:26 +0200

    User class

---
+++ user.py
@@ -0,0 +1,12 @@
+class User():
+    """docstring for User"""
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def take_name(self):
+        return self.name
+
+    def take_email(self):
+        return self.email
+
