e364ff0c4cf2b8c0cfb83457ebe75faf2f00205d
smo93 <georgimpeev@gmail.com>
2014-03-19 09:02:48 +0200

    Added unittests for the merge func.

--- mail_test.py
+++ mail_test.py
@@ -8,6 +8,18 @@ class MailTest(unittest.TestCase):
     def setUp(self):
         self.lists = {1: MailList('list1'), 2: MailList('list2')}
 
+    def test_merge_empty_lists(self):
+        mail.merge(self.lists, 1, 2, 'list3')
+        expected = '[1] - list1\n[2] - list2\n[3] - list3\n'
+        self.assertEqual(expected, mail.show_lists(self.lists))
+
+    def test_merge_non_empty_lists(self):
+        mail.add_new_user(self.lists, 1, 'asd', 'asd')
+        mail.add_new_user(self.lists, 2, 'dsa', 'dsa')
+        mail.merge(self.lists, 1, 2, 'list3')
+        expected = '[1] asd - asd\n[2] dsa - dsa'
+        self.assertEqual(expected, mail.show_list(self.lists, 3))
+
     def test_show_lists(self):
         expected = '[1] - list1\n[2] - list2\n'
         self.assertEqual(expected, mail.show_lists(self.lists))
